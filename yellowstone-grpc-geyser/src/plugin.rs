ff615bd069f63a2b4532184ab0c0a0af53150141
Sergii Grebeniuk <sergii.g@preply.com>
2024-11-03 13:56:50 +0100

    dummy v2.0.14

--- yellowstone-grpc-geyser/src/plugin.rs
+++ yellowstone-grpc-geyser/src/plugin.rs
@@ -23,101 +23,20 @@ use {
     },
 };
 
-#[derive(Debug)]
-pub struct PluginInner {
-    runtime: Runtime,
-    snapshot_channel: Mutex<Option<crossbeam_channel::Sender<Box<Message>>>>,
-    snapshot_channel_closed: AtomicBool,
-    grpc_channel: mpsc::UnboundedSender<Arc<Message>>,
-    grpc_shutdown: Arc<Notify>,
-    prometheus: PrometheusService,
-}
-
-impl PluginInner {
-    fn send_message(&self, message: Message) {
-        if self.grpc_channel.send(Arc::new(message)).is_ok() {
-            MESSAGE_QUEUE_SIZE.inc();
-        }
-    }
-}
-
 #[derive(Debug, Default)]
 pub struct Plugin {
-    inner: Option<PluginInner>,
-}
-
-impl Plugin {
-    fn with_inner<F>(&self, f: F) -> PluginResult<()>
-    where
-        F: FnOnce(&PluginInner) -> PluginResult<()>,
-    {
-        let inner = self.inner.as_ref().expect("initialized");
-        f(inner)
-    }
 }
 
 impl GeyserPlugin for Plugin {
     fn name(&self) -> &'static str {
-        concat!(env!("CARGO_PKG_NAME"), "-", env!("CARGO_PKG_VERSION"))
+        concat!(env!("CARGO_PKG_NAME"), "-dummy-", env!("CARGO_PKG_VERSION"))
     }
 
     fn on_load(&mut self, config_file: &str, is_reload: bool) -> PluginResult<()> {
-        let config = Config::load_from_file(config_file)?;
-
-        // Setup logger
-        solana_logger::setup_with_default(&config.log.level);
-
-        // Create inner
-        let runtime = Builder::new_multi_thread()
-            .thread_name_fn(crate::get_thread_name)
-            .enable_all()
-            .build()
-            .map_err(|error| GeyserPluginError::Custom(Box::new(error)))?;
-
-        let (snapshot_channel, grpc_channel, grpc_shutdown, prometheus) =
-            runtime.block_on(async move {
-                let (debug_client_tx, debug_client_rx) = mpsc::unbounded_channel();
-                let (snapshot_channel, grpc_channel, grpc_shutdown) = GrpcService::create(
-                    config.grpc,
-                    config.block_fail_action,
-                    config.debug_clients_http.then_some(debug_client_tx),
-                    is_reload,
-                )
-                .await
-                .map_err(|error| GeyserPluginError::Custom(format!("{error:?}").into()))?;
-                let prometheus = PrometheusService::new(
-                    config.prometheus,
-                    config.debug_clients_http.then_some(debug_client_rx),
-                )
-                .await
-                .map_err(|error| GeyserPluginError::Custom(Box::new(error)))?;
-                Ok::<_, GeyserPluginError>((
-                    snapshot_channel,
-                    grpc_channel,
-                    grpc_shutdown,
-                    prometheus,
-                ))
-            })?;
-
-        self.inner = Some(PluginInner {
-            runtime,
-            snapshot_channel: Mutex::new(snapshot_channel),
-            snapshot_channel_closed: AtomicBool::new(false),
-            grpc_channel,
-            grpc_shutdown,
-            prometheus,
-        });
-
         Ok(())
     }
 
     fn on_unload(&mut self) {
-        if let Some(inner) = self.inner.take() {
-            inner.grpc_shutdown.notify_one();
-            drop(inner.grpc_channel);
-            inner.prometheus.shutdown();
-            inner.runtime.shutdown_timeout(Duration::from_secs(30));
-        }
     }
 
     fn update_account(
@@ -126,45 +45,13 @@ impl GeyserPlugin for Plugin {
         slot: u64,
         is_startup: bool,
     ) -> PluginResult<()> {
-        self.with_inner(|inner| {
-            let account = match account {
-                ReplicaAccountInfoVersions::V0_0_1(_info) => {
-                    unreachable!("ReplicaAccountInfoVersions::V0_0_1 is not supported")
-                }
-                ReplicaAccountInfoVersions::V0_0_2(_info) => {
-                    unreachable!("ReplicaAccountInfoVersions::V0_0_2 is not supported")
-                }
-                ReplicaAccountInfoVersions::V0_0_3(info) => info,
-            };
 
-            if is_startup {
-                if let Some(channel) = inner.snapshot_channel.lock().unwrap().as_ref() {
-                    let message = Message::Account((account, slot, is_startup).into());
-                    match channel.send(Box::new(message)) {
-                        Ok(()) => MESSAGE_QUEUE_SIZE.inc(),
-                        Err(_) => {
-                            if !inner.snapshot_channel_closed.swap(true, Ordering::Relaxed) {
-                                log::error!(
-                                    "failed to send message to startup queue: channel closed"
-                                )
-                            }
-                        }
-                    }
-                }
-            } else {
-                let message = Message::Account((account, slot, is_startup).into());
-                inner.send_message(message);
-            }
-
-            Ok(())
-        })
+        Ok(())
     }
 
     fn notify_end_of_startup(&self) -> PluginResult<()> {
-        self.with_inner(|inner| {
-            let _snapshot_channel = inner.snapshot_channel.lock().unwrap().take();
-            Ok(())
-        })
+
+        Ok(())
     }
 
     fn update_slot_status(
@@ -173,12 +60,8 @@ impl GeyserPlugin for Plugin {
         parent: Option<u64>,
         status: SlotStatus,
     ) -> PluginResult<()> {
-        self.with_inner(|inner| {
-            let message = Message::Slot((slot, parent, status).into());
-            inner.send_message(message);
-            metrics::update_slot_status(status, slot);
-            Ok(())
-        })
+ 
+        Ok(())
     }
 
     fn notify_transaction(
@@ -186,58 +69,18 @@ impl GeyserPlugin for Plugin {
         transaction: ReplicaTransactionInfoVersions<'_>,
         slot: u64,
     ) -> PluginResult<()> {
-        self.with_inner(|inner| {
-            let transaction = match transaction {
-                ReplicaTransactionInfoVersions::V0_0_1(_info) => {
-                    unreachable!("ReplicaAccountInfoVersions::V0_0_1 is not supported")
-                }
-                ReplicaTransactionInfoVersions::V0_0_2(info) => info,
-            };
 
-            let message = Message::Transaction((transaction, slot).into());
-            inner.send_message(message);
-
-            Ok(())
-        })
+        Ok(())
     }
 
     fn notify_entry(&self, entry: ReplicaEntryInfoVersions) -> PluginResult<()> {
-        self.with_inner(|inner| {
-            #[allow(clippy::infallible_destructuring_match)]
-            let entry = match entry {
-                ReplicaEntryInfoVersions::V0_0_1(_entry) => {
-                    unreachable!("ReplicaEntryInfoVersions::V0_0_1 is not supported")
-                }
-                ReplicaEntryInfoVersions::V0_0_2(entry) => entry,
-            };
 
-            let message = Message::Entry(entry.into());
-            inner.send_message(message);
-
-            Ok(())
-        })
+        Ok(())
     }
 
     fn notify_block_metadata(&self, blockinfo: ReplicaBlockInfoVersions<'_>) -> PluginResult<()> {
-        self.with_inner(|inner| {
-            let blockinfo = match blockinfo {
-                ReplicaBlockInfoVersions::V0_0_1(_info) => {
-                    unreachable!("ReplicaBlockInfoVersions::V0_0_1 is not supported")
-                }
-                ReplicaBlockInfoVersions::V0_0_2(_info) => {
-                    unreachable!("ReplicaBlockInfoVersions::V0_0_2 is not supported")
-                }
-                ReplicaBlockInfoVersions::V0_0_3(_info) => {
-                    unreachable!("ReplicaBlockInfoVersions::V0_0_3 is not supported")
-                }
-                ReplicaBlockInfoVersions::V0_0_4(info) => info,
-            };
 
-            let message = Message::BlockMeta(blockinfo.into());
-            inner.send_message(message);
-
-            Ok(())
-        })
+        Ok(())
     }
 
     fn account_data_notifications_enabled(&self) -> bool {
